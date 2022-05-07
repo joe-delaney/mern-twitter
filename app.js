79ff3afeaa920dbe319d561d3277daf9b94c2642
joe-delaney <joseph.delaney546@gmail.com>
2022-05-07 09:10:15 -0400

    backend user registration complete

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
