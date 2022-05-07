7e7074f3bef34f317eb584c3715d45a38c8c796c
joe-delaney <joseph.delaney546@gmail.com>
2022-05-07 11:06:08 -0400

    finished MERN Twitter

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session/session_reducer';
 import errors from './errors/errors_reducer';
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
     session, 
-    errors
+    errors,
+    tweets: TweetsReducer
 });
 
 export default RootReducer;
